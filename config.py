5a0827ba9e771e11e315e553f4a830e97f175606
nangen34 <nangen34@yahoo.co.jp>
2019-04-10 15:23:26 +0800

    Ver 0.1

---
+++ config.py
@@ -0,0 +1,10 @@
+
+CONSUMER_KEY = "oZ6mJYxmJCBMEync3xoGLKONs"
+CONSUMER_SECRET = "bnLFbKwLqn0g5mvKnJzcZUTTq3OBTXVcvSUDgFOHJf0Nhyxc2t"
+ACCESS_TOKEN = "2473854398-JVTduiO0ZKtS0hGIyquxJTRjJOuiDelhikgWIH7"
+ACCESS_TOKEN_SECRET = "X9kQfxLSvKXU0a57S7uag8I5M0AciDwTzO0rD94ECftc2"
+
+PATH = "D:\Github\get_twitter_home_timeline_pics\_result"
+
+# time interval(second)
+TIMER = 600
--- main.py
+++ main.py
@@ -8,92 +8,68 @@ import time
 import urllib
 from twython import Twython
 
-u"""
-ツイッターで投稿された画像を一括でダウンロードする
-"""
-
-# 画像を保存するディレクトリ(予め作成しておく)
-# 各ユーザの画像は、./images/screen_name/ 内に保存される
-IMAGES_DIR = './'
+# 从config.py中获取文件保存目录
+# Get file save path from config.py
+IMAGES_DIR = config.PATH
+# 从config.py中获取定时器的时间间隔
+# Get the timer interval from config.py
+TIMER = config.TIMER
+# 从config.py中获取Twitter认证信息
+# Get your Twitter key or token etc
 CK = config.CONSUMER_KEY
 CS = config.CONSUMER_SECRET
 ATK = config.ACCESS_TOKEN
 ATS = config.ACCESS_TOKEN_SECRET
-# Twitter 認証関係 https://dev.twitter.com/apps
-# Consumer key
-# CK = 'Consumer keyをここに記入'
-# # Consumer secret
-# CS = 'Consumer secretをここに記入'
-# # Access token
-# ATK = 'Access tokenをここに記入'
-# # Access token secret
-# ATS = 'Access token secretをここに記入'
 
-# 取得するツイート数の最大値の設定(以下の2つの値の積)
-NUM_PAGES = 5  # 取得するページ数
-TWEET_PER_PAGE = 200  # 1ページあたりのツイート数(最大200)
+# NUM_PAGES = 5
+# TWEET_PER_PAGE = 200
 
-# 画像をダウンロードするユーザのスクリーン名を1行に1人ずつ記入する(@の有無は問わない)
-SCREEN_NAMES = '''
-nanyuan_342
-'''
 name_list = []
 
 class TwitterImageDownloader(object):
-    u"""Twitterから画像をダウンロードする"""
 
     def __init__(self):
         super(TwitterImageDownloader, self).__init__()
         self.twitter = Twython(app_key=CK, app_secret=CS, oauth_token=ATK, oauth_token_secret=ATS)
 
 
-    def read_ids(self):
-        ids_all = [line.replace('@', '') for line in SCREEN_NAMES.splitlines() if line]
-        ids = sorted(list(set(ids_all)))
-        return ids
+    # def read_ids(self):
+    #     ids_all = [line.replace('@', '') for line in SCREEN_NAMES.splitlines() if line]
+    #     ids = sorted(list(set(ids_all)))
+    #     return ids
 
     def get_timeline(self):
-        max_id = ''
         url_listx = []
         name_listx = []
-        # for i in range(NUM_PAGES):
         try:
-            # print
-            # 'getting timeline : @', screen_name, (i + 1), 'page'
             tw_result = self.twitter.get_home_timeline(count=200)
-            # tw_result = (
-            #     self.twitter.get_user_timeline(screen_name=screen_name, count=TWEET_PER_PAGE, max_id=max_id)
-            #     if max_id else self.twitter.get_user_timeline(screen_name=screen_name, count=TWEET_PER_PAGE))
             time.sleep(5)
         except Exception as e:
-            print
-            "timeline get error ", e
-            # break
+            print("timeline get error ", e)
         else:
             for result in tw_result:
-                # print(result)
+                # if tweet contain a picture, get it.
                 if 'extended_entities' in result:
-                    # if 'media' in result['extended_entities']:
                     media = result['extended_entities']['media']
-                    print("len", len(media))
+                    print("tweet contain", len(media), ' picture(s)')
                     url_list_multi = []
                     for url in media:
                         url_list_multi.append(url['media_url'])
                     url_listx.append(url_list_multi)
+
+                    # judge if the tweet is RT or not so we can get the correct twitter id.
                     if 'retweeted_status' in result:
                         # print("yes rt")
                         name = result['retweeted_status']['user']['screen_name']
                         name_listx.append(name)
+                        # print(name)
                     else:
                         # print("not rt")
                         name = result['user']['screen_name']
                         name_listx.append(name)
                 else:
                     print("no imgs")
-            # print(len(url_listx),url_listx)
-            # print(len(name_listx),name_listx)
-        # if len(tw_result) < TWEET_PER_PAGE:
-        #     break
+
         return url_listx,name_listx
 
     def create_folder(self, save_dir):
@@ -102,7 +78,8 @@ class TwitterImageDownloader(object):
         except Exception as e:
             print
             'cannot make dir', e
-        # 生成文件列表
+        # 获取文件列表 以便判断是否有重复图片
+        # Get a list of files to determine if there are duplicate images
         file_list = os.listdir(save_dir)
         return file_list
 
@@ -110,26 +87,32 @@ class TwitterImageDownloader(object):
         # print(url)
         for i in range(len(url)):
             url_real = url[i]
-            # print('real',url_real)
+
+            # 截取图片文件名 例如：https://pbs.twimg.com/media/D3uPR1cU0AAuOW9.jpg
+            #———————————————————————————こ↑こ↓————
+            # get file name of pics. For example：https://pbs.twimg.com/media/D3uPR1cU0AAuOW9.jpg
+            # ——————————————————————————— ↑____this part____↑
             file_name_judge = url_real[url_real.rfind('/') + 1:]
-            file_name_file = url_real[url_real.rfind('.') + 1:]
-            # print(file_name_judge)
-            # print(name_listx)
-            file_name = name_listx[j] + "______" + file_name_judge + "." + file_name_file
+
+            #拼接成储存用的最终文件名 例如00unit(作者名)______D3rIFukUIAA7EwF.jpg(文件名)
+            #Create the final file name. For example:00unit(twitter id)______D3rIFukUIAA7EwF.jpg(file names of pics)
+            file_name = name_listx[j] + "______" + file_name_judge
             url_large = '%s:large' % (url_real)
+
+            #判断图片是否重复
+            #determine if there are duplicate images
             result = None
             for i in file_list:
-                # print(file_name_judge, "xxxx", i)
                 result = re.search(file_name_judge, i)
                 if result != None:
                     break
-            print(result)
+            # print(result)
             if result == None:
                 save_path = os.path.join(save_dir, file_name)
                 try:
                     print
                     "download", url_large
-                    print("no problem")
+                    print("download start")
                     url_req = urllib.request.urlopen(url_large)
                 except Exception as e:
                     print(e)
@@ -140,23 +123,17 @@ class TwitterImageDownloader(object):
                     img = open(save_path, 'wb')
                     img.write(img_read)
                     img.close()
-                    time.sleep(0.5)
+                    # time.sleep(0.5)
             else:
-                print("file already exists", file_name)
+                print("file already exists. skip", file_name)
 
-
+    #下载图片
+    #download pics
     def download(self):
-        screen_name_list = self.read_ids()
-        num_users = len(screen_name_list)
-        # print(screen_name_list)
-        # for i, screen_name in enumerate(screen_name_list):
-        save_dir = os.path.join(IMAGES_DIR, 'resultx')
+        save_dir = os.path.join(IMAGES_DIR)
         file_list = self.create_folder(save_dir)
 
         url_list,name_list = self.get_timeline()
-        # print(name_list)
-        # print(url_list)
-        num_urls = len(url_list)
         for j, url in enumerate(url_list):
             self.get_file(url, file_list, save_dir,name_list, url_list, j)
 
@@ -167,12 +144,13 @@ def main():
     tw.download()
     print("finished")
 
+    #定时器
     global timer
-    timer = threading.Timer(600, main)
+    timer = threading.Timer(TIMER, main)
     timer.start()
 
 
 if __name__ == '__main__':
     main()
-    timer = threading.Timer(600, main)
+    timer = threading.Timer(TIMER, main)
     timer.start()
